08e932e0a17f83a3245f10fccbc5df95b5e3114a
AndrewR96 <AndrewRivera968@gmail.com>
2016-02-28 19:54:51 -0500

    First commit

--- TwitterDemo/AppDelegate.swift
+++ TwitterDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,11 +16,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        if User.currentUser != nil{
+            print("There is a current user")
+        
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewControllerWithIdentifier("TweetsNavigationController")
+        
+            window?.rootViewController = vc
+        }
+            NSNotificationCenter.defaultCenter().addObserverForName(User.userDidLogoutNotification, object: nil, queue: NSOperationQueue.mainQueue()) { (NSNotification) -> Void in
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateInitialViewController()
+                self.window?.rootViewController = vc
+                
+                
+                
+        }
+    
+    
         // Override point for customization after application launch.
         return true
-    }
+}
 
     func applicationWillResignActive(application: UIApplication) {
+        
+    
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
@@ -41,6 +63,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool {
+        print(url.description)
+        
+        TwitterClient.sharedInstance.handleOpenUrl(url)
+ 
+            
+        return true
+        
+    }
 }
-
---
+++ TwitterDemo/TweetsViewContoller.swift
@@ -0,0 +1,52 @@
+//
+//  TweetsViewContoller.swift
+//  TwitterDemo
+//
+//  Created by Andrew Rivera on 2/28/16.
+//  Copyright © 2016 Andrew Rivera. All rights reserved.
+//
+
+import UIKit
+
+class TweetsViewContoller: UIViewController {
+
+    var tweets: [Tweet]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        TwitterClient.sharedInstance.homeTimeLine({ (tweets: [Tweet]) -> () in
+            self.tweets = tweets
+            
+            
+            for tweet in tweets{
+                print(tweet.text)
+            }
+            }, failure: {(error: NSError) -> () in
+                print(error.localizedDescription)
+                
+        })
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLogoutButton(sender: AnyObject) {
+    TwitterClient.sharedInstance.logout()
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
